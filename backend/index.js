2a79f5ae179f53fa0a98105e2ca8c7db1f582a30
Roshan Yadav <yroshan504@gmail.com>
2025-12-22 15:53:48 +0530

    port changed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use('/', (req, res) => {
 });
 
 // Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
